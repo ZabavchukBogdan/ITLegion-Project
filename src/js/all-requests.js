b5ba9b2e2facbe1fdfd215402ea5c89bf8fa7c08
A-lex-G <alexey803@gmail.com>
2023-05-12 15:44:30 +0300

    RenderBooks-&-fetchUPD

--- src/js/all-requests.js
+++ src/js/all-requests.js
@@ -21,14 +21,7 @@ export async function getCategories() {
 
 // Повертає об'єкт з властивістю data, його значення = масив обєктів (всі категорії), в кожному обєкті значення властивості books = масив з топовими книгами
 export async function getTopBooks() {
-  try {
-    const response = await axios.get(`${BASE_URL}${topBooksEndpoint}`);
-    // Тестовий лог____________
-    console.log(response.data);
-    // ________________________
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
+  const response = await axios.get(`${BASE_URL}${topBooksEndpoint}`);
+  const topBooksArr = response.data.flatMap(item => item.books);
+  return topBooksArr;
 }
