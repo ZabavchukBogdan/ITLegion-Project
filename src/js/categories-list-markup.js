0ef23785b3eb818e2e493cd4ee0db3800d2e2e50
ZabavchukBogdan <117076795+ZabavchukBogdan@users.noreply.github.com>
2023-05-14 22:02:06 +0300

    update shop.js

--- src/js/categories-list-markup.js
+++ src/js/categories-list-markup.js
@@ -1,34 +1,3 @@
-// import { getCategoriesList } from './all-requests';
-
-// const categoriesList = document.querySelector('.sidebar__book-categories-list');
-
-// export function markupCategoriesList(categories) {
-//     return categories.map(({list_name}) =>
-//     `<li class="category-list-item">
-//         <button type="button" class="category">
-//             ${list_name}
-//         </button>
-//     </li>`).join("");
-// }
-
-// getCategoriesList().then(catList => {
-//     const categories =
-//         `<li class="category-list-item first-elem-js">
-//         <button type="button" class="category all-categories">
-//             All categories
-//         </button>
-//     </li>` + markupCategoriesList(catList);
-//     categoriesList.innerHTML = categories;
-// }).catch(err => {
-//     categoriesList.innerHTML =
-//         `<li class="category-list-item">
-//             <p class="categories-err">
-//                 The list of categories is empty
-//             </p>
-//         </li>`;
-//     console.log(err);
-// });
-
 
 import {
   getCategoriesList,
