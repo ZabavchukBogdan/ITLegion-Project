3a7bf927a4a7c5faeeea4f68f0592d0e653a4202
Tetiana Omanashvili <tanya.nvt@gmail.com>
2023-05-15 00:41:55 +0300

    fixed styles

--- src/js/markup-card-shopping-list.js
+++ src/js/markup-card-shopping-list.js
@@ -7,19 +7,31 @@ const bookdeskIconUrl = new URL(
 );
 
 const amazonIcon = new URL(
-  '../images/bookshops/amazonmob@1x.png',
-  import.meta.url
-);
-const appleBooksIcon = new URL(
-  '../images/bookshops/applebookmob@1x.png',
-  import.meta.url
-);
-const bookshopIcon = new URL(
-  '../images/bookshops/bookshopmob@1x.png',
+  '../images/bookshops/amazon@1x.png',
   import.meta.url
 );
 
-const arrIconsShop = [amazonIcon, appleBooksIcon, bookshopIcon];
+const amazonIconRet = new URL(
+  '../images/bookshops/amazon@2x.png',
+  import.meta.url
+);
+
+const appleIcon = new URL('../images/bookshops/apple@1x.png', import.meta.url);
+
+const appleIconRet = new URL(
+  '../images/bookshops/apple@2x.png',
+  import.meta.url
+);
+
+const booksIcon = new URL(
+  '../images/bookshops/bookshop@1x.png',
+  import.meta.url
+);
+
+const booksIconRet = new URL(
+  '../images/bookshops/bookshop@2x.png',
+  import.meta.url
+);
 
 export function markupBookForShoppingList({
   _id,
@@ -30,38 +42,11 @@ export function markupBookForShoppingList({
   author,
   buy_links,
 }) {
-  let buyLinksMarkup = '';
-  const links = buy_links.filter(({ name }) =>
-    ['Amazon', 'Apple Books', 'Bookshop'].includes(name)
-  );
-
-  for (let i = 0; i < links.length; i++) {
-    buyLinksMarkup += `<li><a href="${links[i].url}">
-      <img
-      src="${arrIconsShop[i]}"
-      alt="amazon"
-      class="shop-item-shopping-list"
-      width="16"
-      height="16"
-    /></a></li>`;
-  }
-
-  // for (const link of links) {
-  //   buyLinksMarkup += `<li><a href="${link.url}">
-  //   <img
-  //   src="${amazonIcon}"
-  //   alt="amazon"
-  //   class="shop-item-shopping-list"
-  //   width="32"
-  //   height="11"
-  // /></a></li>`;
-  // }
-
   return `<div class="wrapper-shopping-list">
   <div class="wrapper-img-shopping-list"><img src="${
     book_image ||
     'https://storage.googleapis.com/du-prd/books/images/9780063226050.jpg'
-  }" alt="${title}" width="100" height="142"></div>
+  }" alt="${title}"></div>
     <div class="wrapper-text-shopping-list"> 
     <h2 class="title-book-shopping-list">${title}</h2>
     <h3 class="category-book-shopping-list">${list_name}</h3>
@@ -72,7 +57,49 @@ export function markupBookForShoppingList({
     <div class="bottom-wrapper-shopping-list">
       <p class="author-book-shopping-list">${author}</p>
       <ul class="shops-shopping-list">
-        ${buyLinksMarkup}
+        <li>
+          <a href="${
+            buy_links[0].url
+          }" class="shop-item-shopping-list" target="_blank">
+            <img
+              srcset="${amazonIcon} 1x, ${amazonIconRet} 2x"
+              src="${amazonIcon}"
+              alt="Amazon"
+              class="amazon-image-shopping-list"
+              width="48"
+              height="15"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="${
+            buy_links[1].url
+          }" class="shop-item-shopping-list" target="_blank">
+            <img
+              srcset="${appleIcon} 1x, ${appleIconRet} 2x"
+              src="${appleIcon}"
+              alt="Apple Book"
+              class="apple-image-shopping-list"
+              width="28"
+              height="27"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="${
+            buy_links[4].url
+          }" class="shop-item-shopping-list" target="_blank">
+            <img
+              srcset="${booksIcon} 1x, ${booksIconRet} 2x"
+              src="${booksIcon}"
+              alt="Bookshop"
+              class="books-image-shopping-list"
+              width="32"
+              height="30"
+            />
+          </a>
+        </li>
+
       </ul>
     </div></div>
     <button class="remove-from-shopping-list" data-book-id="${_id}">
