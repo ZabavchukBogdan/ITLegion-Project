c243736184317b8e1ce3cd1fb78879fe4d78ab92
ZabavchukBogdan <117076795+ZabavchukBogdan@users.noreply.github.com>
2023-05-14 11:25:07 +0300

    delet index js code

--- src/index.js
+++ src/index.js
@@ -14,39 +14,3 @@ import './js/markup-card-shopping-list';
 
 
 
-// import fetchBook from './js/shopping-list';
-
-
-// import { supportMarkup } from './js/support-ukraine';
-
-// supportMarkup();
-
-// import { getCategories, getTopBooks } from './js/all-requests';
-// import { createBookCard } from './js/homepage-books-render';
-
-
-
-// import {markupCategoriesList} from './js/categories-list-markup';
-
-// const categoriesList = document.querySelector('.sidebar__book-categories-list');
-
-// //rendering list of book categories
-// getCategories().then(catList => {
-//     const categories = 
-//     `<li class="category-list-item first-elem-js">
-//         <button type="button" class="category all-categories">
-//             All categories
-//         </button>
-//     </li>` + markupCategoriesList(catList);
-//     categoriesList.innerHTML = categories;
-//     }).catch(err => {
-//         categoriesList.innerHTML = 
-//         `<li class="category-list-item">
-//             <p class="categories-err">
-//                 The list of categories is empty
-//             </p>
-//         </li>`;
-//         console.log(err);
-//     })
-
-// import './js/support-ukraine';
