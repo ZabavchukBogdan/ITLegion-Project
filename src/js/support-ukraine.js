51bc4908cd09ffe87b7a8379cec78da7ac0c7b24
ZabavchukBogdan <117076795+ZabavchukBogdan@users.noreply.github.com>
2023-05-14 12:21:23 +0300

    add support icons

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -1,6 +1,85 @@
 
 import Swiper from 'swiper';
-import { supportArray } from './suport-list';
+
+
+
+const savethechildren = require('../images/support/savethechildren.png');
+const savethechildren_2x = require('../images/support/savethechildren@2x.png');
+const projecthope = require('../images/support/projecthope.png');
+const projecthope_2x = require('../images/support/projecthope@2x.png');
+const united24 = require('../images/support/united24.png');
+const united24_2x = require('../images/support/united24@2x.png');
+const internationalmedicalcorps = require('../images/support/internationalmedicalcorps.png');
+const internationalmedicalcorps_2x = require('../images/support/internationalmedicalcorps@2x.png');
+const msf = require('../images/support/msf.png');
+const msf_2x = require('../images/support/msf@2x.png');
+const razom = require('../images/support/razom.png');
+const razom_2x = require('../images/support/razom@2x.png');
+const actionagainsthunger = require('../images/support/actionagainsthunger.png');
+const actionagainsthunger_2x = require('../images/support/actionagainsthunger@2x.png');
+const wvi = require('../images/support/wvi.png');
+const wvi_2x = require('../images/support/wvi@2x.png');
+const prytulafoundation = require('../images/support/prytulafoundation.png');
+const prytulafoundation_2x = require('../images/support/prytulafoundation@2x.png');
+
+export const supportArray = [
+    {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: savethechildren ,
+    img2x: savethechildren_2x,
+   
+  },
+    {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+      img: projecthope,
+    img2x: projecthope_2x,
+  },
+    {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+      img: united24,
+    img2x: united24_2x,
+      },
+    {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+      img: internationalmedicalcorps,
+    img2x: internationalmedicalcorps_2x,
+  },
+    {
+    title: 'Medecins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+      img: msf,
+    img2x: msf_2x,
+    
+  },
+    {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+      img: razom,
+    img2x: razom_2x,
+  },
+    {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+      img: actionagainsthunger,
+    img2x: actionagainsthunger_2x,
+  },
+    {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+      img: wvi,
+    img2x: wvi_2x,
+  },
+    {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+      img: prytulafoundation,
+    img2x: prytulafoundation_2x,
+  },
+];
 
 const supportList = document.querySelector('.js-support-list');
 function markupList(arr) {
@@ -25,89 +104,4 @@ export function supportMarkup() {
 
 
 supportMarkup();
-
-// const savethechildren = require('../images/support/savethechildren.png');
-
-// const supportList = document.querySelector('.js-support-list');
-// function renderSupportList(arr) {
-//     const marcup = arr.map(({ img, url, title, id }) =>
-//         `<li class="support-li">
-//     <a class="support-link" href="${url}">
-//     <span>${id}</span>
-//     <img class="support-img" scr="${img}" alt="${title}"></a></li>`)
-//         .join('');
-//     return marcup;
-// }
-// export function supportMarkup() {
-//     supportList.insertAdjacentHTML('beforeend', renderSupportList(supportArray));
-//     initScrollButton('.swiper', '.support-list');
-// }
-
-// export const supportArray = [
-//     {
-//       id: "01",
-//     title: 'Save the Children',
-//     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-//     img: savethechildren ,
-//     // img2x: './images/support/savethechildren@2x.png'
-   
-//   },
-//     {
-//       id: "02",
-//     title: 'Project HOPE',
-//     url: 'https://www.projecthope.org/country/ukraine/',
-//       img: './images/support/projecthope.png',
-//     img2x: './images/support/projecthope@2x.png',
-//   },
-//     {
-//         id: "03",
-//     title: 'UNITED24',
-//     url: 'https://u24.gov.ua/uk',
-//       img: './images/support/UNITED24.png',
-//     img2x: './images/support/UNITED24@2x.png',
-//       },
-//     {
-//         id: "04",
-//     title: 'International Medical Corps',
-//     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-//       img: './images/support/internationalmedicalcorps.png',
-//     img2x: './images/support/internationalmedicalcorps@2x.png',
-//   },
-//     {
-//         id: "05",
-//     title: 'Medecins Sans Frontieres',
-//     url: 'https://www.msf.org/ukraine',
-//       img: './images/support/msf.png',
-//     img2x: './images/support/msf@2x.png',
-    
-//   },
-//     {
-//         id: "06",
-//     title: 'RAZOM',
-//     url: 'https://www.razomforukraine.org/',
-//       img: '../images/support/razom.png',
-//     img2x: '../images/support/razom@2x.png'
-//   },
-//     {
-//         id: "07",
-//     title: 'Action against hunger',
-//     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-//       img: './images/support/actionagainsthunger.png',
-//     img2x: './images/support/actionagainsthunger@2x.png',
-//   },
-//     {
-//         id: "08",
-//     title: 'World vision',
-//     url: 'https://www.wvi.org/emergencies/ukraine',
-//       img: './images/support/wvi.png',
-//     img2x: './images/support/wvi@2x.png',
-//   },
-//     {
-//         id: "09",
-//     title: 'Serhiy Prytula Charity Foundation',
-//     url: 'https://prytulafoundation.org/en',
-//       img: './images/support/Prytula.png',
-//     img2x: './images/support/prytulafoundation@2x.png',
-//   },
-// ];
 
