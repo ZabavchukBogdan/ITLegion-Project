a408078b37b5d598e4106f12af0dd1d07e9d0d54
Kostyantyn <118232131+folkUA@users.noreply.github.com>
2023-05-14 20:31:02 -0400

    1st commit

--- src/js/autorization-modal.js
+++ src/js/autorization-modal.js
@@ -11,6 +11,7 @@ import {
 } from './helpers/autorization/autorization-marcup.js';
 
 const modal = document.querySelector('.js_autorization_modal');
+const mainBody = document.querySelector('body');
 
 //  кнопка створення форми авторизації
 const button = document.querySelector('.js_signup_btn');
@@ -18,21 +19,18 @@ button.addEventListener('click', onBtnClick);
 
 function onBtnClick(evt) {
   evt.preventDefault();
+  if (modal.classList.contains('burger')) {
+    modal.classList.toggle('burger');
+    toggleBurger();
+  }
   modal.innerHTML = '';
+  mainBody.classList.add('stop-scrolling');
   modal.classList.add('selected');
   modal.innerHTML = createMarcupSignUp(themeSet());
   closeModalBtn();
   signUpForm();
 }
 
-const logOut = document.querySelector('.js_logout_btn');
-logOut.addEventListener('click', onLogOut, { once: true });
-function onLogOut(evt) {
-  evt.preventDefault();
-  modal.innerHTML = '';
-  localStorage.setItem('autorized', false);
-  localStorage.removeItem('userName');
-}
 ///////////////
 
 function signUpForm() {
@@ -128,12 +126,13 @@ function autorizationForm() {
       Notify.info("Невірно введені ім'я або пароль");
       return;
     }
-
     localStorage.setItem('autorized', true);
     localStorage.setItem('userName', nickname);
     form.reset();
     form.removeEventListener('submit', onSubmit);
+
     modal.innerHTML = '';
+    mainBody.classList.remove('stop-scrolling');
     modal.classList.remove('selected');
   }
 }
@@ -142,6 +141,7 @@ function closeModalBtn() {
   closeBtn.addEventListener('click', onClose, { once: true });
   function onClose(evt) {
     evt.preventDefault();
+    mainBody.classList.remove('stop-scrolling');
     modal.innerHTML = '';
     modal.classList.remove('selected');
   }
@@ -153,4 +153,98 @@ function themeSet() {
     theme = '';
   }
   return theme;
+}
+
+//////////////////////// Burger Menu
+const burgerMenuBtn = document.querySelector('.js-open-menu');
+
+burgerMenuBtn.addEventListener('click', onClickBurg);
+function onClickBurg(evt) {
+  evt.preventDefault();
+  const userName = localStorage.getItem('userName');
+  const shopBasketUrl = new URL('../images/symbol-defs.svg', import.meta.url);
+  const portraitUrl = new URL('../images/symbol-defs.svg', import.meta.url);
+  const arrowRightIconUrl = new URL(
+    '../images/symbol-defs.svg',
+    import.meta.url
+  );
+  arrowRightIconUrl.hash = 'icon-arrow-right';
+  portraitUrl.hash = 'icon-user';
+  shopBasketUrl.hash = 'icon-shop';
+
+  modal.classList.toggle('burger');
+  modal.innerHTML = '';
+
+  if (modal.classList.contains('burger')) {
+    if (localStorage.getItem('autorized') === 'true') {
+      modal.innerHTML = `<div class="burger_logout_box"><div class="user-main">
+      <svg class="logout_svg_name" width="20px" height="20px">
+        <use class="icon-href-burger-user" href="${portraitUrl}"></use>
+      </svg>
+      <p class="user-name">${userName}</p>
+    </div>
+    <div class="logout_links_box">
+      <a class="logout_home_link">HOME</a>
+      <a class="shop_logout_link"
+        >SHOPING LIST
+        <svg class="logout_svg-shop" width="20px" height="20px">
+          <use
+            class="icon-href-burger-shoplist"
+            href="${shopBasketUrl}"
+          ></use></svg
+      ></a>
+    </div>
+    <button type="button" class="js_logout_btn_burg auth__modal-open-burg logout_btn_burg">
+          Log out   
+          <svg class="logout_svg" width="20px" height="20px">
+          <use href="${arrowRightIconUrl}"></use>
+        </svg>    
+          </button></div>`;
+
+      const logOut = document.querySelector('.js_logout_btn_burg');
+
+      logOut.addEventListener('click', onLogOut, { once: true });
+    } else {
+      modal.innerHTML = `<button type="button"  class=" auth__modal-open-burg js_signup_btn_burg">
+              Sign up 
+            <svg class="" width="20px" height="20px">
+              <use href="${arrowRightIconUrl}"></use>
+            </svg>  
+            </button>`;
+      const button = document.querySelector('.js_signup_btn_burg');
+      button.addEventListener('click', onBtnClick, { once: true });
+    }
+  }
+  // Приховування свг іконки
+  toggleBurger();
+}
+
+function toggleBurger() {
+  const burger = burgerMenuBtn.children[0].children[0];
+  const xClosed = burgerMenuBtn.children[0].children[1];
+  if (modal.classList.contains('burger')) {
+    // припинення скролу застосовуеться на боді
+    mainBody.classList.add('stop-scrolling');
+    modal.classList.add('container');
+
+    burger.style.visibility = 'hidden';
+    xClosed.style.visibility = 'visible';
+  } else {
+    mainBody.classList.remove('stop-scrolling');
+    modal.classList.remove('container');
+    burger.style.visibility = 'visible';
+    xClosed.style.visibility = 'hidden';
+  }
+}
+
+function onLogOut(evt) {
+  evt.preventDefault();
+  if (modal.classList.contains('burger')) {
+    modal.classList.toggle('burger');
+    toggleBurger();
+  }
+
+  modal.innerHTML = '';
+  localStorage.setItem('autorized', false);
+  localStorage.removeItem('userName');
 }
